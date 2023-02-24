09ef9ef5a159d7c522cef88da1d28f5abdb2020e
mwyzer <muhammad.wyzer@gmail.com>
2023-02-24 17:03:17 +0700

    add screenshot

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistation from './pages/coaches/CoachRegistration.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
@@ -17,13 +17,13 @@ const router = createRouter({
       component: CoachDetail,
       props: true,
       children: [
-        { path: 'contact', component: ContactCoach } // /coaches/c1/contact
-      ]
+        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+      ],
     },
-    { path: '/register', component: CoachRegistation },
+    { path: '/register', component: CoachRegistration },
     { path: '/requests', component: RequestsReceived },
-    { path: '/:notFound(.*)', component: NotFound }
-  ]
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
 });
 
 export default router;
